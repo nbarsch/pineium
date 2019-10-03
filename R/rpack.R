d6521f2860aff5f68c8346bfbe75ced82951ccec
nbarsch <nbarsch@colgate.edu>
2019-10-03 05:20:49 -0600

    rpack
    rpack

--- R/rpack.R
+++ R/rpack.R
@@ -4,11 +4,12 @@
 #' @param package_vector String vector of packages to check for and install if missing
 #' @export
 rpack <- function(package_vector=c("data.table","dplyr","stringr","qdapRegex","foreach","lubridate")){
-  suppressWarnings(if(!("foreach" %in%installed.packages())){install.packages("foreach")})
+  suppressWarnings(if(!("foreach" %in% as.character(installed.packages()))){install.packages("foreach",repos='http://cran.us.r-project.org')})
+  library(foreach)
   foreach(ipv=1:length(package_vector))%do%{
     tempx <- as.character(package_vector[ipv])
-    suppressWarnings(if(!(tempx %in%installed.packages())){install.packages(as.character(tempx))})
-    reqinstall <- tempx %in% installed.packages()
-    if(isTRUE(requinstall)){print(paste0(as.character(tempx)," SUCCESSFULLY LOADED"))}else{print(paste0(as.character(tempx)," FAILED"))}
+    suppressWarnings(if(!(tempx %in% as.character(data.frame(installed.packages())$Package))){install.packages(as.character(tempx),repos='http://cran.us.r-project.org')})
+    reqinstall <- tempx %in% as.character(installed.packages())
+    if(isTRUE(reqinstall)){print(paste0(as.character(tempx)," SUCCESSFULLY LOADED"))}else{print(paste0(as.character(tempx)," FAILED"))}
   }
 }
