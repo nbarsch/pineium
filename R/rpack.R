8eb50f55dc0764b22f2d853d252a90e9800c44e9
nbarsch <nbarsch@colgate.edu>
2019-10-03 05:57:49 -0600

    rpack
    rpack

--- R/rpack.R
+++ R/rpack.R
@@ -3,16 +3,22 @@
 #' Check for and install vector of packages if missing
 #' @param package_vector String vector of packages to check for and install if missing
 #' @export
-rpack <- function(package_vector=c("data.table","dplyr","stringr","qdapRegex","foreach","lubridate")){
+rpack <- function(package_vector=c("data.table","dplyr","stringr","readr","qdapRegex","rvest","httr","RSelenium","XML","xml2")){
+  if(!all(as.logical(sapply(as.character(package_vector),FUN=function(x){x%in%data.frame(installed.packages())$Package})))){
   suppressWarnings(if(!("foreach" %in% as.character(installed.packages()))){install.packages("foreach",repos='http://cran.us.r-project.org')})
-  library(foreach)
-  foreach(ipv=1:length(package_vector))%do%{
+  for(ipv in c(1:length(package_vector))){
     tempx <- as.character(package_vector[ipv])
-    suppressWarnings(if(!(tempx %in% as.character(data.frame(installed.packages())$Package))){install.packages(as.character(tempx),repos='http://cran.us.r-project.org')})
+    if(length(tempx)>1){tempx <- tempx[1]}
+    suppressWarnings(if(!(tempx %in% as.character(data.frame(installed.packages())$Package))){install.packages(as.character(tempx))})
     reqinstall <- tempx %in% as.character(installed.packages())
     if(isTRUE(reqinstall)){
       print(paste0(as.character(tempx)," SUCCESSFULLY LIBRARIED"))
-      library(as.character(tempx))
+      library(tempx,character.only=TRUE)
       }else{print(paste0(as.character(tempx)," FAILED"))}
+  }
+  }else{
+    for(ip in c(1:length(package_vector))){
+      library(package_vector[ip],character.only = T)
+    }
   }
 }
