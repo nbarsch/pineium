605e8662cae5259f6fc9c69b8e485bab6af59be8
nbarsch <nbarsch@colgate.edu>
2019-10-03 06:03:28 -0600

    rpack
    rpack

--- R/rpack.R
+++ R/rpack.R
@@ -3,7 +3,8 @@
 #' Check for and install vector of packages if missing
 #' @param package_vector String vector of packages to check for and install if missing
 #' @export
-rpack <- function(package_vector=c("data.table","dplyr","stringr","readr","qdapRegex","rvest","httr","RSelenium","XML","xml2")){
+rpack <- function(package_vector=""){
+  if(package_vector==""){package_vector<-c("data.table","dplyr","stringr","readr","qdapRegex","rvest","httr","RSelenium","XML","xml2")}
   if(!all(as.logical(sapply(as.character(package_vector),FUN=function(x){x%in%data.frame(installed.packages())$Package})))){
   suppressWarnings(if(!("foreach" %in% as.character(installed.packages()))){install.packages("foreach",repos='http://cran.us.r-project.org')})
   for(ipv in c(1:length(package_vector))){
@@ -18,7 +19,11 @@ rpack <- function(package_vector=c("data.table","dplyr","stringr","readr","qdapR
   }
   }else{
     for(ip in c(1:length(package_vector))){
-      library(package_vector[ip],character.only = T)
+      reqinstall <- package_vector[ip] %in% as.character(installed.packages())
+      if(isTRUE(reqinstall)){
+        print(paste0(package_vector[ip]," SUCCESSFULLY LIBRARIED"))
+        library(package_vector[ip],character.only=TRUE)
+      }else{print(paste0(package_vector[ip]," FAILED"))}
     }
   }
 }
