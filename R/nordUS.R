ec669008cc9a99922a15679965f7371e0a74d59b
Neal Barsch <nbarsch@colgate.edu>
2019-10-24 03:02:36 -0600

    Create nordUS.R

---
+++ R/nordUS.R
@@ -0,0 +1,18 @@
+#' nordUS data
+#'
+#' lazy load data
+#'
+#' @docType data
+#'
+#' @usage data(nordUS)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#'
+#' @source \href{https://api.nordvpn.com/server}{NORD API}
+#'
+#' @examples
+#' data(nordUS)
+"nordUS"
