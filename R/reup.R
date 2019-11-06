3ddd25b20a07e405667aaa9721a103ae7687f1ba
Neal Barsch <nealbotn@gmail.com>
2019-11-06 16:36:13 -0700

    Update reup.R

--- R/reup.R
+++ R/reup.R
@@ -2,10 +2,10 @@
 #'
 #' restart R, uninstall, and reinstall a package
 #' @param package package to reinstall
-#' @param github_user github user to install package from if installing from github (to install from CRAN, set github_user=NA) (default: nbarsch)
+#' @param github_user github user to install package from if installing from github (to install from CRAN, set github_user=NA) (default: nealbotn)
 #' @param auth_token github authentication token if applicable for private github packages
 #' @export
-reup <- function(package="webbium", github_user="nbarsch",auth_token=NA, lib="default"){
+reup <- function(package="webbium", github_user="nealbotn",auth_token=NA, lib="default"){
   depack()
   Sys.sleep(1)
   if(lib=="default"){lib<-.libPaths()[1]}
@@ -25,3 +25,4 @@ reup <- function(package="webbium", github_user="nbarsch",auth_token=NA, lib="de
 
 
 
+.rs.restartR()
